0db9283aa46fdec593d785c636990e442ad81cad
Developer Mohamed hemidan <moheynasir@gmail.com>
2026-04-12 19:43:14 +0200

    Create models/Cart.js with mongoose schema for shopping cart

---
+++ models/Cart.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    products: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        }
+    }],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
